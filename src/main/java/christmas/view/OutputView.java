45388bf68b36a9859b1c60a5d7a3bcf3d346c94e
혜민 <alddlv81@gmail.com>
2023-11-11 20:11:33 +0900

    feat: 금액 출력시 세자리마다 , 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -10,7 +10,7 @@ public class OutputView {
 
     public static void printTotalPrice(OrderMenu orderMenu){
         System.out.println("\n<할인 전 총주문 금액>");
-        System.out.println(orderMenu.calculateTotalPrice() +"원");
+        System.out.println(String.format("%,d", orderMenu.calculateTotalPrice())+"원");
     }
 
     public static void printGiftMenu(OrderMenu orderMenu){
