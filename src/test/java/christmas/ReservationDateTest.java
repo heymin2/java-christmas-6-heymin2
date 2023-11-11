a0b8ad4ded05e9c36e492a71386266844754ee3a
혜민 <alddlv81@gmail.com>
2023-11-12 02:10:01 +0900

    test(ReservationDateTest): 날짜 입력 범위 테스트로 합침

--- src/test/java/christmas/ReservationDateTest.java
+++ src/test/java/christmas/ReservationDateTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ReservationDateTest {
-
     @DisplayName("날짜가 숫자가 아닐 경우 예외 처리")
     @ValueSource(strings = {"a", "1a", " "})
     @ParameterizedTest
@@ -18,19 +17,10 @@ class ReservationDateTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-
-    @DisplayName("날짜가 1 미만일 경우 예외 처리")
-    @ValueSource(strings = {"0", "-5"})
+    @DisplayName("날짜가 1~31사이 범위가 아닐 경우 예외 처리")
+    @ValueSource(strings = {"0", "-5", "32", "100"})
     @ParameterizedTest
     void validateDateIsUnderOne(String input) {
-        assertThatThrownBy(() -> new ReservationDate(input))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("날짜가 31 초과일 경우 예외 처리")
-    @ValueSource(strings = {"32", "100"})
-    @ParameterizedTest
-    void validateDateIsAboveThirtyOne(String input) {
         assertThatThrownBy(() -> new ReservationDate(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
