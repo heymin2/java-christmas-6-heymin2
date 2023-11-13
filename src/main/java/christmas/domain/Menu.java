f38e0c6d56eeb35c872a3d5f947f62ceee561f4b
혜민 <alddlv81@gmail.com>
2023-11-13 13:08:04 +0900

    remove: Menu 클래스의 fromMenuName 메소드에서 불필요한 예외 처리 삭제

--- src/main/java/christmas/domain/Menu.java
+++ src/main/java/christmas/domain/Menu.java
@@ -47,6 +47,6 @@ public enum Menu {
                 return menu;
             }
         }
-        throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        return null;
     }
 }
