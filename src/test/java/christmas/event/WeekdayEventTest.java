55b78fa2a72cac61efb4be8e9ced30672ed161aa
혜민 <alddlv81@gmail.com>
2023-11-14 11:40:38 +0900

    test: 주중 이벤트 테스트 추가

---
+++ src/test/java/christmas/event/WeekdayEventTest.java
@@ -0,0 +1,31 @@
+package christmas.event;
+
+import christmas.domain.event.WeekdayEvent;
+import christmas.domain.order.OrderItem;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WeekdayEventTest {
+    private final List<OrderItem> orderItems = List.of(new OrderItem("초코케이크", 2));
+
+    @DisplayName("주중일 경우, 할인 금액 출력")
+    @ValueSource(ints = {17, 27})
+    @ParameterizedTest
+    public void inWeekday(int reservationDate) {
+        WeekdayEvent weekdayEvent = new WeekdayEvent(reservationDate, orderItems);
+        assertThat(weekdayEvent.calculateDiscount()).isEqualTo(4046);
+    }
+
+    @DisplayName("주중이 아닐 경우, '0' 출력")
+    @ValueSource(ints = {1, 30})
+    @ParameterizedTest
+    public void notInWeekday(int reservationDate) {
+        WeekdayEvent weekdayEvent = new WeekdayEvent(reservationDate, orderItems);
+        assertThat(weekdayEvent.calculateDiscount()).isEqualTo(0);
+    }
+}
